3c9845be3c5ebff936f25e08b99af2ded93fe700
Nagord <46509577+Nagord@users.noreply.github.com>
2026-06-02 20:57:19 -0700

    Add project files.

---
+++ DisableCalamities/Patch.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+
+namespace DisableCalamities
+{
+    [HarmonyPatch(typeof(EncounterCollection), "GetRandomEncounter")]
+    internal class Patch
+    {
+        static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
---
+++ DisableCalamities/Plugins.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using VoidManager;
+using VoidManager.MPModChecks;
+
+namespace DisableCalamities
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.USERS_PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("Void Crew.exe")]
+    [BepInDependency(VoidManager.MyPluginInfo.PLUGIN_GUID)]
+    public class BepinPlugin : BaseUnityPlugin
+    {
+        internal static ManualLogSource Log;
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "N/A")]
+        private void Awake()
+        {
+            // Configure Unity profile to modding profile.
+            RuntimeSettings.UseModsProfile = true;
+
+            Log = Logger;
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+
+
+    public class VoidManagerPlugin : VoidPlugin
+    {
+        public override MultiplayerType MPType => MultiplayerType.All;
+
+        public override string Author => MyPluginInfo.PLUGIN_AUTHORS;
+
+        public override string Description => MyPluginInfo.PLUGIN_DESCRIPTION;
+
+        public override string ThunderstoreID => MyPluginInfo.PLUGIN_THUNDERSTORE_ID;
+    }
+}
